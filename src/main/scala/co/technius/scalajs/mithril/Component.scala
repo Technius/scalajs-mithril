d6d1eaf034a91763dedcf4c29b0fe79b15fad20e
Technius <techniux@gmail.com>
2015-09-06 16:50:14 -0700

    Add component class for view-only components

--- src/main/scala/co/technius/scalajs/mithril/Component.scala
+++ src/main/scala/co/technius/scalajs/mithril/Component.scala
@@ -4,8 +4,15 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
 @JSExportDescendentObjects
-trait Component {
+trait ViewComponent {
+  @JSExport def view(): js.Object
+}
+
+trait Component extends ViewComponent {
   type Controller
   @JSExport def controller(): Controller
   @JSExport def view(ctrl: Controller): js.Object
+
+  override def view(): js.Object = throw new IllegalArgumentException()
 }
+
--- src/main/scala/co/technius/scalajs/mithril/package.scala
+++ src/main/scala/co/technius/scalajs/mithril/package.scala
@@ -6,9 +6,13 @@ import scala.scalajs.js
 package object mithril {
   val m = Mithril
 
-  implicit def scalaToMithrilComponent(c: Component): MithrilComponent = {
-    val ctrl: js.Function = () => c.controller()
-    val view: js.Function = (ctrl: c.Controller) => c.view(ctrl)
-    js.Dynamic.literal(controller = ctrl, view = view).asInstanceOf[MithrilComponent]
+  implicit def scalaToMithrilComponent(comp: ViewComponent): MithrilComponent = comp match {
+    case c: Component =>
+      val ctrl: js.Function = () => c.controller()
+      val view: js.Function = (ctrl: c.Controller) => c.view(ctrl)
+      js.Dynamic.literal(controller = ctrl, view = view).asInstanceOf[MithrilComponent]
+    case c: ViewComponent =>
+      val view: js.Function = () => c.view()
+      js.Dynamic.literal(view = view).asInstanceOf[MithrilComponent]
   }
 }
