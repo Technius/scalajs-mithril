4497eb64c1a511c580144f3379987b8dda48fa4c
Technius <techniux@gmail.com>
2016-10-27 20:12:21 -0700

    Add m.trust (for real, this time)

--- core/src/main/scala/co/technius/scalajs/mithril/Mithril.scala
+++ core/src/main/scala/co/technius/scalajs/mithril/Mithril.scala
@@ -6,7 +6,7 @@ import scala.scalajs.js.|
 import scala.scalajs.js.annotation.JSName
 
 @js.native @JSName("m")
-object Mithril extends MithrilCore with MithrilRendering {
+object Mithril extends MithrilCore with MithrilRendering with MithrilHtml {
   def prop[A](): MithrilProp[A] = js.native
   def prop[A](value: A): MithrilProp[A] = js.native
 
@@ -43,4 +43,10 @@ trait MithrilRendering extends js.Object {
   def redraw(forceSync: Boolean): Unit = js.native
   def startComputation(): Unit = js.native
   def endComputation(): Unit = js.native
+}
+
+@js.native
+trait MithrilHtml extends js.Object {
+  // Must return js.Object, since returning String causes a runtime error
+  def trust(string: String): js.Object = js.native
 }
