5e5e229767b4820593c61016c28bdc4d55ccff8e
Bryan Tan <techniux@gmail.com>
2017-06-08 12:31:43 -0700

    Update routing facade; use routes in the examples

--- core/src/main/scala/co/technius/scalajs/mithril/Route.scala
+++ core/src/main/scala/co/technius/scalajs/mithril/Route.scala
@@ -5,19 +5,24 @@ import scala.scalajs.js
 import scala.scalajs.js.|
 import scala.scalajs.js.annotation.ScalaJSDefined
 
+import MithrilRoute.Route
+
 @js.native
 trait MithrilRoute extends js.Object {
-  import MithrilRoute.Route
 
   def get(): String = js.native
 
-  def set(path: String, data: js.Object, options: js.Object): Unit = js.native
+  def set(path: String, data: js.Object = ???, options: js.Object = ???): Unit = js.native
 
-  def link(vnode: VNode): js.Function1[dom.Event, Unit] = js.native
+  def link(vnode: VNode): js.Function1[VNode, Unit] = js.native
 
   def apply(root: dom.Element, defaultRoute: String, routes: js.Dictionary[Route]): Unit = js.native
 
   def prefix(prefix: String): Unit = js.native
+
+  def param(): String = js.native
+
+  def param(key: String): js.Dictionary[js.Any] = js.native
 
   var mode: String = js.native
 }
@@ -28,15 +33,45 @@ trait MithrilRoute extends js.Object {
  */
 @ScalaJSDefined
 abstract class RouteResolver extends js.Object {
-  def onmatch(resolve: js.Function1[Component[_, _], Unit], args: js.Object,
-      requestPath: String): Unit = ()
 
-  def render(vnode: VNode): VNode = vnode
+  // explicit type annotations for Scala 2.11 support
+  // could be implemented in 2.12 using
+  //     (_, _) => ()
+  def onmatch: js.Function2[js.Dictionary[js.Any], String, RouteResolver.Resolved] =
+    (_: js.Dictionary[js.Any], _: String) => (): RouteResolver.Resolved
+
+  // explicit type annotations for Scala 2.11 support
+  // could be implemented in 2.12 using
+  //     identity
+  def render: js.Function1[VNode, VNode] = (vnode: VNode) => vnode
+}
+
+object RouteResolver  {
+
+  /**
+    * A resolved component returned by onmatch.
+    */
+  type Resolved = Component[_,_] | js.Promise[Component[_,_]] | Unit
+
+  /**
+    * Creates a RouteResolver that contains a render method.
+    */
+  def render(f: VNode => VNode): Route = new RouteResolver {
+    override def render = f
+  }
+
+  /**
+    * Creates a RouteResolver that contains an onmatch method.
+    */
+  def onmatch(f: (js.Dictionary[js.Any], String) => Resolved): Route =
+    new RouteResolver {
+      override def onmatch = f
+    }
 }
 
 object MithrilRoute {
   type Route = Component[_, _] | RouteResolver
-  @inline implicit class RichMithrilRoute(val route: MithrilRoute) extends AnyVal {
+  @inline implicit class MithrilRouteOps(val route: MithrilRoute) extends AnyVal {
     @inline def apply(rootElement: dom.raw.Element, defaultRoute: String)(routes: (String, Route)*): Unit = {
       route(rootElement, defaultRoute, js.Dictionary(routes: _*))
     }
--- examples/src/main/scala/ExampleApp.scala
+++ examples/src/main/scala/ExampleApp.scala
@@ -6,35 +6,78 @@ import co.technius.scalajs.mithril._
 
 object ExampleApp extends js.JSApp {
   def main(): Unit = {
-    m.mount(dom.document.getElementById("app"), Showcase.component)
+    // Build routes based on demo choices
+    val routes =
+      DemoDropdown.choices map { case (_, link, compF) =>
+        // We want to include the demo selection combobox above the displayed
+        // demo, so a RouteResolver is used to customize how each route is
+        // displayed.
+        val view = RouteResolver.render { vnode =>
+
+          // Create the component using the factory function
+          val comp = compF()
+
+          // Create the view
+          m.fragment(new js.Object, js.Array[VNode](
+            m(DemoDropdown.component),
+            comp
+          ))
+        }
+
+        // Map each route to the view
+        link -> view
+      }
+
+    m.route(dom.document.getElementById("app"), "/", js.Dictionary(routes: _*))
   }
 }
 
-object Showcase {
+/**
+  * Contains the combobox component that is used to select which demo to view by
+  * changing the route.
+  */
+object DemoDropdown {
 
+  /**
+    * Component to display when no demo is selected
+    */
   val defaultComponent = Component.viewOnly[js.Object] { _ =>
     m("p", "Select an example to display!")
   }
 
-  val component = Component.stateful[State, js.Object](_ => new State) { vnode =>
-    import vnode.state
+  /**
+    * List of all of the demos shown in this app. They are organized by name,
+    * link, and a factory function that produces the component.
+    */
+  val choices = Seq[(String, String, () => VNode)](
+    ("None", "/", () => m(defaultComponent)),
+    ("Hello", "/hello", () => m(HelloDemo.component)),
+    ("Counter", "/counter", () => m(CounterDemo.component)),
+    ("Tree", "/tree", () => m(TreeDemo.component, TreeDemo.Attrs("root"))),
+    ("Data Fetch", "/data-fetch", () => m(DataFetchDemo.component)),
+    ("Scalatags Demo", "/scalatags", () => m(ScalatagsDemo.component))
+  )
 
-    val default = m(defaultComponent)
-    val choices = Seq[(String, VNode)](
-      "None" -> default,
-      "Hello" -> m(HelloDemo.component),
-      "Counter" -> m(CounterDemo.component),
-      "Tree" -> m(TreeDemo.component, TreeDemo.Attrs("root")),
-      "Data Fetch" -> m(DataFetchDemo.component),
-      "Scalatags Demo" -> m(ScalatagsDemo.component)
-    )
+  /**
+    * The combobox component
+    */
+  val component = Component.viewOnly[js.Object] { vnode =>
 
-    val choiceList = choices map { case (n, _) =>
-      m("option", js.Dynamic.literal("value" -> n), n)
+    val currentRoute = m.route.get()
+
+    // Build the list of demos
+    val choiceList = choices map { case (name, link, _) =>
+      val attrs = js.Dynamic.literal("value" -> name)
+      if (currentRoute == link) {
+        attrs.selected = true
+      }
+      m("option", attrs, name)
     }
 
+    // Change route when a demo is selected
     val selectHandler: js.ThisFunction = (e: dom.raw.HTMLOptionElement) => {
-      state.selection() = e.value
+      val linkOpt = choices.collectFirst { case (name, link, _) if name == e.value => link }
+      m.route.set(linkOpt.getOrElse("/"))
     }
 
     val choiceBox = m("select", js.Dynamic.literal(
@@ -42,13 +85,6 @@ object Showcase {
       "onchange" -> selectHandler
     ), choiceList.toJSArray)
 
-    m.fragment(js.Dynamic.literal(), js.Array[VNode](
-      choiceBox,
-      choices.collectFirst({ case (n, s) if n == state.selection() => s }).getOrElse(default)
-    ))
-  }
-
-  protected class State {
-    val selection = MithrilStream[String]("None")
+    choiceBox
   }
 }
